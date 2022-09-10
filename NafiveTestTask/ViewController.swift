8f6204417a0cdae4fd284a733657d2b3371de9ab
VasilkovaIvanna <ivanna.vasilkova@wolkov.consulting>
2022-09-10 19:56:33 +0300

    Initial Commit

---
+++ NafiveTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NafiveTestTask
+//
+//  Created by Ivy on 10.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
